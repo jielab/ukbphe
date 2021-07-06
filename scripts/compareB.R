c2582fabb44b155bad38da5763536d184b31b773
Jie Huang <jiehuang001@gmail.com>
2021-07-06 09:54:46 +0800

    JH update

--- scripts/compareB.R
+++ scripts/compareB.R
@@ -1,10 +1,13 @@
+## 如果进行比较的两个文件有一个文件太大（几百万行），会导致R读进去很忙，这个时候先用下面的 Linux 代码把大的那个文件变小
+## trait1=XXX; trait2=YYY; zcat $trait2.gwas.txt | fgrep -wf $trait1.top.snps > $trait1.$trait2.tmp
+
 setwd("C:/Users/jiehu/Desktop")
 source("D:/scripts/library/compareB.f.R")
 
 pdf("compareB.pdf")
 par(mfrow=c(2,2), mai=c(1,1,0.5,0.5))
-compareP(
-  f1="D:/data/gwas/posCtrls/cad.w.t2d.txt", f1_name="CAD", f1_snp="SNP", f1_ea="EA", f1_nea="NEA", f1_eaf="EAF", f1_beta="BETA", f1_se="SE", f1_p="P",
-  f2="D:/data/gwas/posCtrls/cad.w.t2d.txt", f2_name="T2D", f2_snp="SNP", f2_ea="EA", f2_nea="NEA", f2_eaf="t2d.EAF", f2_beta="t2d.BETA", f2_se="SE", f2_p="t2d.P"
+compareB(
+  f1="D:/data/gwas/you/income.top.txt", f1_name="income", f1_snp="SNP", f1_ea="EA", f1_nea="NEA", f1_eaf=NA, f1_beta="BETA", f1_se="SE", f1_p="P",
+  f2="D:/analysis/you/income.intel.tmp", f2_name="intel", f2_snp="SNP", f2_ea="A1", f2_nea="A2", f2_eaf=NA, f2_beta="BETA", f2_se="SE", f2_p="P"
 )
 dev.off()
